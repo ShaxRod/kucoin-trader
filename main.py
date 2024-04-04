4cde97e51e97e6c455127a9b641f9aea92b43832
Shaqir <shaxrodgers@gmail.com>
2024-04-04 23:33:09 +0200

    added prediction backtesting in main script. LSTM is operational. Now we need to fine tune her.

--- main.py
+++ main.py
@@ -6,19 +6,57 @@ import pandas as pd
 import modules.general as gen
 from modules.kucoin_helpers import KC
 from modules.portfolio import Optimise
+from modules.forecast import Forecast
+
+api_params = gen.read_json('authentication/shax_kucoin.json')
+df_params = gen.read_json('paramaters/pandas_dataframes.json')
+portfolio_params = gen.read_json('paramaters/portfolio.json')
 
 
-def main():
-    api_params = gen.read_json('authentication/shax_kucoin.json')
-    df_params = gen.read_json('paramaters/pandas_dataframes.json')
-    portfolio_params = gen.read_json('paramaters/portfolio.json')
-
+def portfolio_optimisation():
     portfolio = Optimise(portfolio_params=portfolio_params, api_params=api_params, df_params=df_params)
     portfolio.get_assets()
     simulations = portfolio.monte_carlo(n=10000, rfr=0.6)
     frame = pd.DataFrame(simulations).T
     optimised_weights = frame[frame['sharpe_ratio'] == frame['sharpe_ratio'].max()]['weights']
 
+
+def price_forecasting():
+    backtest = 60
+    models = dict()
+    now = datetime.datetime.now()
+    delta_t = now - datetime.timedelta(days=365)
+    for asset in portfolio_params:
+        data = KC(api_params).historical_data_frame(portfolio_params[asset],
+                                                    '1day',
+                                                    gen.to_unix_time(delta_t),
+                                                    gen.to_unix_time(now),
+                                                    df_params)
+        lstm_model = Forecast(data[['close']], time_step=60)
+        lstm_model.lstm()
+        predictions = []
+        for i in range(0, backtest):
+            if i == 0:
+                p = lstm_model.predict(data[['close']][-60 - i:])
+            else:
+                p = lstm_model.predict(data[['close']][-60 - i:- i])
+            predictions.append(p[0][0])
+        actual = data[['time', 'close']][-backtest+1:]
+        actual['predictions'] = predictions[:-1]
+
+        models[asset] = actual
+
+
+    for asset in models:
+        plt.figure(figsize=(20, 8))
+        plt.plot(pd.to_datetime(models[asset]['time']), models[asset]['close'],
+                 pd.to_datetime(models[asset]['time']), models[asset]['predictions'])
+        plt.legend(['actual closing price', 'predicted closing price'])
+        plt.title(f'LSTM forward looking outcomes: {asset}')
+        plt.xlabel('date')
+        plt.ylabel('closing price')
+        plt.savefig(f'C:/Users/User/OneDrive/Documents/python outputs/stock forecasts/plots/{asset}.png')
+        plt.close()
 
 if __name__ == '__main__':
     main()
--- modules/forecast.py
+++ modules/forecast.py
@@ -1,54 +1,62 @@
 import numpy as np
 import pandas as pd
+
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from statsmodels.tsa.arima.model import ARIMA
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 
+import os
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
 
 class Forecast:
 
     def __init__(self, data: pd.DataFrame, time_step: int):
         self.__X = np.array([])
-        self.__Y = np.array([])
+
+        self._X = np.array([])
+        self._Y = np.array([])
 
         self.asset = data
         self.time = time_step
-
-        self.lstm = None
+        self.model = None
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
 
     def create_dataset(self):
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        scaled_data = scaler.fit_transform(self.asset)
+        scaled_data = self.scaler.fit_transform(self.asset)
         X, Y = [], []
         for i in range(len(scaled_data) - self.time - 1):
             X.append(scaled_data[i:(i + self.time), 0])
             Y.append(scaled_data[i + self.time, 0])
 
-        self.__X = np.array(X)
-        self.__Y = np.array(Y)
+        self._X = np.array(X)
+        self._Y = np.array(Y)
 
     def lstm(self):
-
         self.create_dataset()
-        self.__X = self.__X.reshape(self.__X.shape[0], self.__X.shape[1], 1)
+        self.__X = self._X.reshape(self._X.shape[0], self._X.shape[1], 1)
 
         # LSTM Model
-        model = Sequential()
-        model.add(LSTM(50, return_sequences=True, input_shape=(self.time, 1)))
-        model.add(LSTM(50, return_sequences=False))
-        model.add(Dense(25))
-        model.add(Dense(1))
+        lstm = Sequential()
+        lstm.add(LSTM(50, return_sequences=True, input_shape=(self.time, 1)))
+        lstm.add(LSTM(50, return_sequences=False))
+        lstm.add(Dense(25))
+        lstm.add(Dense(1))
 
-        model.compile(optimizer='adam', loss='mean_squared_error')
+        lstm.compile(optimizer='adam', loss='mean_squared_error')
 
         # Train the model
-        model.fit(self.__X, self.__Y, batch_size=32, epochs=100, verbose=1)
+        lstm.fit(self.__X, self._Y, batch_size=32, epochs=100, verbose=1)
 
-        self.lstm = model
+        self.model = lstm
 
-
-
-
-
+    def predict(self, input_data):
+        scaled_input = self.scaler.transform(input_data)
+        prediction = self.model.predict(np.reshape(scaled_input,
+                                                   (1, len(input_data), 1)
+                                                   )
+                                        )
+        return self.scaler.inverse_transform(prediction)
