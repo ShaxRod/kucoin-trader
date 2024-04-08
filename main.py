4350972e18a40de83cf9466e9e391642d5dbe011
Shaqir <shaxrodgers@gmail.com>
2024-04-08 18:41:42 +0200

    Added lagged variables and daily volatility.

--- main.py
+++ main.py
@@ -22,30 +22,41 @@ def portfolio_optimisation():
 
 
 def price_forecasting():
-    backtest = 60
+    backtest = 120
     models = dict()
     now = datetime.datetime.now()
-    delta_t = now - datetime.timedelta(days=365)
+    delta_t = now - datetime.timedelta(days=int(365.3*3))
     for asset in portfolio_params:
         data = KC(api_params).historical_data_frame(portfolio_params[asset],
                                                     '1day',
                                                     gen.to_unix_time(delta_t),
                                                     gen.to_unix_time(now),
                                                     df_params)
-        lstm_model = Forecast(data[['close']], time_step=60)
+        lstm_model = Forecast(data, 'close', time_step=60)
         lstm_model.lstm()
         predictions = []
         for i in range(0, backtest):
-            if i == 0:
-                p = lstm_model.predict(data[['close']][-60 - i:])
+            if lstm_model.rolling_window > lstm_model.lag_count:
+                padding = lstm_model.rolling_window
             else:
-                p = lstm_model.predict(data[['close']][-60 - i:- i])
-            predictions.append(p[0][0])
-        actual = data[['time', 'close']][-backtest+1:]
+                padding = lstm_model.lag_count
+            index = 60 + padding + i
+            index = index * - 1
+            if i == 0:
+                input_t = data[index:]
+                input_object = Forecast(input_t, 'close', time_step=60)
+                input_object.pipeline()
+                p = lstm_model.predict(input_data=input_object.data.drop(columns=['close']))
+            else:
+                input_t = data[index:- i]
+                input_object = Forecast(input_t, 'close', time_step=60)
+                input_object.pipeline()
+                p = lstm_model.predict(input_data=input_object.data.drop(columns=['close']))
+            predictions.append(p)
+        actual = data[['time', 'close']][-backtest + 1:]
         actual['predictions'] = predictions[:-1]
 
         models[asset] = actual
-
 
     for asset in models:
         plt.figure(figsize=(20, 8))
@@ -58,5 +69,6 @@ def price_forecasting():
         plt.savefig(f'C:/Users/User/OneDrive/Documents/python outputs/stock forecasts/plots/{asset}.png')
         plt.close()
 
+
 if __name__ == '__main__':
-    main()
+    price_forecasting()
--- modules/forecast.py
+++ modules/forecast.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import LSTM, Dropout, Dense
 from statsmodels.tsa.arima.model import ARIMA
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
@@ -14,49 +14,96 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 class Forecast:
 
-    def __init__(self, data: pd.DataFrame, time_step: int):
-        self.__X = np.array([])
+    def __init__(self, data: pd.DataFrame, kline: str, time_step: int):
 
-        self._X = np.array([])
-        self._Y = np.array([])
+        self.__X = None
+        self.__Y = data[[kline]]
+        self.__X_scaler = MinMaxScaler()
+        self.__Y_scaler = MinMaxScaler()
 
-        self.asset = data
+        self.data = data[[kline]]
         self.time = time_step
+        self.kline = kline
         self.model = None
-        self.scaler = MinMaxScaler(feature_range=(0, 1))
+        self.lag_count = 0
+        self.rolling_window = 0
+
+    def pipeline(self):
+
+        if len(self.data.columns) > 1:
+            self.data = self.__Y.copy(deep=True)
+
+        self.lags()
+        self.volatility()
+        self.data = self.data.dropna()
+        self.__X = self.data.drop(columns=self.kline)
+        # Drop NaN values that are a result of lagging and rolling calculations
+
+    def lags(self, lags: int = 12):
+        for i in range(1, lags + 1):
+            self.data[f'close_lag_{i}'] = self.__Y.shift(i)
+        self.lag_count = lags
+
+    def volatility(self, rolling_window: int = 20):
+        self.data['daily_returns'] = self.__Y.pct_change()
+        self.data['volatility'] = self.data['daily_returns'].rolling(window=rolling_window).std()
+        self.rolling_window = rolling_window
 
     def create_dataset(self):
-        scaled_data = self.scaler.fit_transform(self.asset)
-        X, Y = [], []
-        for i in range(len(scaled_data) - self.time - 1):
-            X.append(scaled_data[i:(i + self.time), 0])
-            Y.append(scaled_data[i + self.time, 0])
+        self.__X = self.__X_scaler.fit_transform(self.__X)
+        self.__Y = self.__Y_scaler.fit_transform(self.__Y)
 
-        self._X = np.array(X)
-        self._Y = np.array(Y)
+        X_data, Y_data = [], []
+        for i in range(len(self.__X) - self.time - 1):
+            X_data.append(self.__X[i:(i + self.time), :])
+            Y_data.append(self.__Y[i + self.time, 0])
+
+        self.__X = np.array(X_data)
+        self.__Y = np.array(Y_data)
 
     def lstm(self):
+
+        self.pipeline()
         self.create_dataset()
-        self.__X = self._X.reshape(self._X.shape[0], self._X.shape[1], 1)
+        # Reshape X for LSTM [samples, time steps, features]
+        self.__X = self.__X.reshape(self.__X.shape[0],
+                                    self.time,
+                                    -1)
 
         # LSTM Model
         lstm = Sequential()
-        lstm.add(LSTM(50, return_sequences=True, input_shape=(self.time, 1)))
-        lstm.add(LSTM(50, return_sequences=False))
+        lstm.add(LSTM(50,
+                      return_sequences=True,
+                      input_shape=(self.time,
+                                   self.__X.shape[2]
+                                   )
+                      )
+                 )
+
+        lstm.add(LSTM(50,
+                      return_sequences=False
+                      )
+                 )
         lstm.add(Dense(25))
+        lstm.add(Dropout(0.2))
         lstm.add(Dense(1))
 
-        lstm.compile(optimizer='adam', loss='mean_squared_error')
+        lstm.compile(optimizer='adam',
+                     loss='mean_squared_error')
 
         # Train the model
-        lstm.fit(self.__X, self._Y, batch_size=32, epochs=100, verbose=1)
+        lstm.fit(self.__X, self.__Y, batch_size=32, epochs=100, verbose=1)
 
         self.model = lstm
 
     def predict(self, input_data):
-        scaled_input = self.scaler.transform(input_data)
-        prediction = self.model.predict(np.reshape(scaled_input,
-                                                   (1, len(input_data), 1)
-                                                   )
-                                        )
-        return self.scaler.inverse_transform(prediction)
+        scaled_input = self.__X_scaler.transform(input_data)
+        prediction_scaled = self.model.predict(np.reshape(scaled_input,
+                                                          (1,
+                                                           input_data.shape[0],
+                                                           -1
+                                                           )
+                                                          )
+                                               )
+        prediction = self.__Y_scaler.inverse_transform(prediction_scaled)
+        return prediction[0, 0]
